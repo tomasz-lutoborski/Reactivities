4005b17767f47b716f39fddf93de11ad50c7a50a
tomasz-lutoborski <tomek218@hotmail.com>
2022-05-24 16:31:14 +0200

    db model

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,17 +11,18 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Persistance;
 
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,6 +33,10 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite();
+            })
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
