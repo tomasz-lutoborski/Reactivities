eec972d353f46e2ae252640799437a07cc354698
tomasz-lutoborski <tomek218@hotmail.com>
2022-05-25 15:03:31 +0200

    commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,8 +35,8 @@ namespace API
             });
             services.AddDbContext<DataContext>(opt =>
             {
-                opt.UseSqlite();
-            })
+                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
